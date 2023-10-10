9d94c474ff0de0a31fbe8149d759d7822685f0e5
Nirmitk Tripathii <nirmitktripathii@yahoo.com>
2023-10-11 04:10:40 +0530

    Model Training added.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from plant_disease_classification import logger
 from plant_disease_classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from plant_disease_classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-# from plant_disease_classification.pipeline.stage_03_training import ModelTrainingPipeline
+from plant_disease_classification.pipeline.stage_03_training import ModelTrainingPipeline
 # from plant_disease_classification.pipeline.stage_04_evaluation import EvaluationPipeline
 
 
@@ -32,16 +32,16 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Training"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    model_trainer = ModelTrainingPipeline()
-#    model_trainer.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
