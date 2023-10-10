e88dfa2083d2650550d7a72955e4b058fa5975d5
Nirmitk Tripathii <nirmitktripathii@yahoo.com>
2023-10-11 02:26:05 +0530

    Base Model preparation completed.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from plant_disease_classification import logger
 from plant_disease_classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from plant_disease_classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from plant_disease_classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 # from plant_disease_classification.pipeline.stage_03_training import ModelTrainingPipeline
 # from plant_disease_classification.pipeline.stage_04_evaluation import EvaluationPipeline
 
@@ -18,16 +18,16 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Prepare base model"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    prepare_base_model = PrepareBaseModelTrainingPipeline()
-#    prepare_base_model.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
