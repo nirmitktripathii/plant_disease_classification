3ed5c94e3eb5daf4df8a73f8ae975e6a32f803e4
Nirmitk Tripathii <nirmitktripathii@yahoo.com>
2023-10-11 18:43:57 +0530

    Flask App created

--- src/plant_disease_classification/components/data_ingestion.py
+++ src/plant_disease_classification/components/data_ingestion.py
@@ -35,3 +35,8 @@ class DataIngestion:
         os.makedirs(unzip_path, exist_ok=True)
         with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
             zip_ref.extractall(unzip_path)
+
+        os.remove(self.config.local_data_file)
+
+    import os
+
